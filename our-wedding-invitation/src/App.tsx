9a8e2523b918972e4a947bb13027bbb9a6f78d42
Chris Choi <162276106+Chrois27@users.noreply.github.com>
2024-10-10 17:58:27 +0900

    1.24.1010

--- our-wedding-invitation/src/App.tsx
+++ our-wedding-invitation/src/App.tsx
@@ -1,45 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App: React.FC = () => {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
   useEffect(() => {
     const preventDefault = (e: Event) => {
       if (e.type === 'wheel' && !(e as WheelEvent).ctrlKey) {
-        return; // 일반 스크롤은 허용
+        return;
       }
       e.preventDefault();
     };
 
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+      setWindowHeight(window.innerHeight);
+    };
+
+    const handleScroll = () => {
+      if (window.innerHeight !== windowHeight) {
+        setViewportHeight();
+      }
+    };
+
+    setViewportHeight();
+
+    window.addEventListener('resize', setViewportHeight);
+    window.addEventListener('scroll', handleScroll);
     document.addEventListener('gesturestart', preventDefault, { passive: false });
     document.addEventListener('gesturechange', preventDefault, { passive: false });
     document.addEventListener('gestureend', preventDefault, { passive: false });
     document.addEventListener('wheel', preventDefault, { passive: false });
 
-    // 뷰포트 높이 설정 함수
-    const setViewportHeight = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
-
-    // 초기 뷰포트 높이 설정
-    setViewportHeight();
-
-    // 리사이즈 이벤트에 대한 뷰포트 높이 조정
-    window.addEventListener('resize', setViewportHeight);
-
     return () => {
+      window.removeEventListener('resize', setViewportHeight);
+      window.removeEventListener('scroll', handleScroll);
       document.removeEventListener('gesturestart', preventDefault);
       document.removeEventListener('gesturechange', preventDefault);
       document.removeEventListener('gestureend', preventDefault);
       document.removeEventListener('wheel', preventDefault);
-      window.removeEventListener('resize', setViewportHeight);
     };
-  }, []);
+  }, [windowHeight]);
 
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{ minHeight: `${windowHeight}px` }}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
